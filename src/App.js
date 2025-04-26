5d5270cdac943ca0ba3bd3925fcc5214e72a9e81
TurahRae <Turahrae@gmail.com>
2025-04-26 19:25:28 +0100

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 import './App.css';
 
  function App() {
   return (
     <>
-    <h1 className='active'>Turah</h1>
-    <p>Learning React</p>
+    <Header />
+    <div>
+    <h1>Learning React</h1>
+    </div>
+    <Footer />
       </>
 
   );
