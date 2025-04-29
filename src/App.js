16d7033741b422dc620750614dca20719e96fedb
TurahRae <Turahrae@gmail.com>
2025-04-29 18:24:53 +0100

    new-addition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import { Header } from './components/Header';
-import { Footer } from './components/Footer';
-import './App.css';
+// import { Header } from './components/Header';
+// import { Footer } from './components/Footer';
+// import './App.css';
 
- function App() {
+//  function App() {
+//   return (
+//     <>
+//     <Header />
+//     <div>
+//     <h1>Learning React</h1>
+//     </div>
+//     <Footer />
+//       </>
+
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import './App.css'
+
+export default function App() {
+    let count = 0;
+
+    function handleAdd(){
+        count+=1;
+        console.log(count);
+    }
   return (
-    <>
-    <Header />
-    <div>
-    <h1>Learning React</h1>
-    </div>
-    <Footer />
-      </>
 
+    <div className='App'>
+        <div className='box'>
+        <p>{count}</p>
+            <button onClick={handleAdd}className='add'>ADD</button>
+            <button className='sub'>SUB</button>
+        </div>
+    </div>
   );
 }
-
-export default App;
