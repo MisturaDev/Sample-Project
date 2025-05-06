b1beddae5ddba251cd5140c376fd291d5d5439e4
TurahRae <Turahrae@gmail.com>
2025-05-06 18:22:33 +0100

    conditional-useState

--- src/App.js
+++ src/App.js
@@ -103,3 +103,38 @@ return(
   </div>
 )
 };
+
+import React from 'react'
+import { useState } from 'react';
+import './App.css'
+
+export default function App() {
+  const[tasks, setTasks] = useState([
+    {id:2025, name: "Record React Lectures", completed: true},
+    {id:2026, name: "Edit React Lectures", completed: false},
+    {id:2027, name: "Watch React Lectures", completed: true},
+    {id:2028, name: "Turah Rae React", completed: true},
+    {id:2029, name: "Olanrewaju React", completed: false}
+  ]);
+
+  const [show, setShow] = useState(false);
+
+  function handleDelete(id){
+    setTasks (tasks.filter(task => task.id !== id));
+  }
+
+return(
+  <div className='App'>
+    <h1>Task List</h1>
+    <ul>
+      <button className="trigger" onClick={() => setShow(!show)}>Toggle</button>
+      { show && tasks.map((task) => (
+        <li key={task.id} className={task.completed ? "completed" : "incomplete" }>
+          <span>{task.id} - {task.name} </span>
+          <button onClick={() => handleDelete(task.id)}className='delete'>Delete</button>
+          </li>
+      )) }
+    </ul>
+  </div>
+);
+}
