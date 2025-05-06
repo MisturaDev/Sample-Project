22a563f72f91121cfca8e4810db589af60004a4b
TurahRae <Turahrae@gmail.com>
2025-05-06 16:06:33 +0100

    useState

--- src/App.js
+++ src/App.js
@@ -13,28 +13,65 @@
 //       </>
 
 //   );
-// }
 
+// }
 // export default App;
 
+// import React from 'react'
+// import { useState } from 'react';
+// import './App.css'
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function handleAdd(){
+//     setCount(count + 1);
+//   }
+
+//   function handleSub(){
+//     setCount(count - 1);
+//   }
+// return (
+
+//     <div className='App'>
+//         <div className='box'>
+//         <p>{count}</p>
+//             <button onClick={handleAdd}className='add'>ADD</button>
+//             <button onClick={handleAdd}className='sub'>SUB</button>
+//         </div>
+//     </div>
+//   );
+// }
+
 import React from 'react'
+import { useState } from 'react';
 import './App.css'
 
 export default function App() {
-    let count = 0;
+    const [count, setCount] = useState (0);
 
     function handleAdd(){
-        count+=1;
-        console.log(count);
+        setCount (count => count + 1);
+        setCount (count => count + 1);
+        setCount (count => count + 1);
     }
+
+    function handleSub(){
+      setCount (count - 1);
+        }
+
+        function hadleReset(){
+          setCount (0);
+        }
   return (
 
     <div className='App'>
         <div className='box'>
         <p>{count}</p>
             <button onClick={handleAdd}className='add'>ADD</button>
-            <button className='sub'>SUB</button>
+            <button onClick={handleSub}className='sub'>SUB</button>
+            <button onClick={hadleReset}className='reset'>RESET</button>
         </div>
     </div>
   );
 }
+
